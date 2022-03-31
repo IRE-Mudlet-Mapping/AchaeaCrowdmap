e97b50f4235bde26ba56254eed74fe745fc5af94
keneanung <keneanung@googlemail.com>
2022-03-31 22:51:09 +0200

    Remove json map and generate it ourselves

--- map-utilities/generate-map.js
+++ map-utilities/generate-map.js
@@ -2,4 +2,6 @@ const { MudletMapReader } = require("mudlet-map-binary-reader");
 
 const inputFile = "./Map/map";
 const mapData = MudletMapReader.read(inputFile);
+MudletMapReader.exportJson(mapData, './Map/map.json', false)
+MudletMapReader.exportJson(mapData, './Map/map_mini.json', true)
 MudletMapReader.export(mapData, './Map/')
