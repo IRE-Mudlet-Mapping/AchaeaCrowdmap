bd3070777a0bbe347c52d7490c1cbd1027d846a6
keneanung <keneanung@googlemail.com>
2022-04-04 21:17:34 +0200

    Generate JSON file for GH actions
    The JSON map files used in GH actions are now automatically generated. This
    allows us to get rid of the checked-in version in a follow up PR. We use a
    two-step procedure to avoid GH actions that run from the target branch erroring
    out on the missing file.

--- map-utilities/generate-map.js
+++ map-utilities/generate-map.js
@@ -1,5 +1,16 @@
 const { MudletMapReader } = require("mudlet-map-binary-reader");
+const yargs = require("yargs");
 
-const inputFile = "./Map/map";
+const argv = yargs.option("base-dir", {
+  default: ".",
+  type: "string",
+  description: "The base directory to use the Maps directory from",
+})
+.help()
+.argv;
+
+const inputFile = `${argv.baseDir}/Map/map`;
 const mapData = MudletMapReader.read(inputFile);
-MudletMapReader.export(mapData, './Map/')
+MudletMapReader.exportJson(mapData, `${argv.baseDir}/Map/map.json`, false)
+MudletMapReader.exportJson(mapData, `${argv.baseDir}/Map/map_mini.json`, true)
+MudletMapReader.export(mapData, `${argv.baseDir}/Map/`)
